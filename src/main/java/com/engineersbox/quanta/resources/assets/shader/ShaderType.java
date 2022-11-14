335ebb4e3e507fa3566026b45f3403a53f7d4b2c
EngineersBox <toastcraft.info@gmail.com>
2022-11-14 16:49:54 +1100

    Added tessellation evaluation & control shaders and compute shader support

--- src/main/java/com/engineersbox/quanta/resources/assets/shader/ShaderType.java
+++ src/main/java/com/engineersbox/quanta/resources/assets/shader/ShaderType.java
@@ -4,12 +4,16 @@ import static org.lwjgl.opengl.GL20.GL_FRAGMENT_SHADER;
 import static org.lwjgl.opengl.GL20.GL_VERTEX_SHADER;
 import static org.lwjgl.opengl.GL32.GL_GEOMETRY_SHADER;
 import static org.lwjgl.opengl.GL40.GL_TESS_CONTROL_SHADER;
+import static org.lwjgl.opengl.GL40.GL_TESS_EVALUATION_SHADER;
+import static org.lwjgl.opengl.GL43.GL_COMPUTE_SHADER;
 
 public enum ShaderType {
     VERTEX(GL_VERTEX_SHADER),
     FRAGMENT(GL_FRAGMENT_SHADER),
     GEOMETRY(GL_GEOMETRY_SHADER),
-    TESSELLATION(GL_TESS_CONTROL_SHADER);
+    TESSELLATION_EVALUATION(GL_TESS_EVALUATION_SHADER),
+    TESSELLATION_CONTROL(GL_TESS_CONTROL_SHADER),
+    COMPUTE(GL_COMPUTE_SHADER);
 
     private final int type;
 
